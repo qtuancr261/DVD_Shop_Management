63e914c26e4068ddf6efa2098c79157ac7682aaa
thieuquangtuan <tuancr261@gmail.com>
2017-10-08 14:44:55 +0700

    Thêm hàm xử lý đăng nhập

--- web/js/processData.js
+++ web/js/processData.js
@@ -11,7 +11,7 @@ function checkInfoBeforeCreateAccount()
     var userPhone = document.getElementById("userPhone").value;
     var userAddress = document.getElementById("userAddress").value;
     if (userName == "" || userEmail == "" || userPWD == "" || userPhone == "" || userAddress == "")
-        alert("Error");
+        alert("Vui lòng nhập đầy đủ thông tin để đăng ký tài khoản...");
     else
     {
         var xmlhttp = new XMLHttpRequest(); //dung de gui DL den webserver
@@ -24,8 +24,45 @@ function checkInfoBeforeCreateAccount()
         var rs = xmlhttp.responseText.trim();
         if (rs === "OK")
             alert("Đăng ký tài khoản thành công.");
-        else 
+        else
             alert("Email bạn nhập vào đã được sử dụng, vui lòng kiểm tra lại...");
+    }
+}
+
+function checkInfoBeforeLogin()
+{
+    var userEmail = document.getElementById("userEmailLogin").value;
+    var userPWD = document.getElementById("userPWDLogin").value;
+    if (userEmail == "" || userPWD == "")
+        alert("Bạn vui lòng nhập đầy đủ thông tin để đăng nhập");
+    else
+    {
+        var xmlhttp = new XMLHttpRequest(); //dung de gui DL den webserver
+        xmlhttp.onreadystatechange = function () { // listen for state changes
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200 && xmlhttp.responseText.trim() == "OK") 
+            { // when completed we can move away
+                window.location = "index.html";
+            }
+            else if (xmlhttp.readyState == 4 && xmlhttp.status == 200 && xmlhttp.responseText.trim() == "ERORR")
+            {
+                alert("Đăng nhập thất bại, vui lòng kiểm tra lại thông tin...");
+            }
+            
+        }
+        xmlhttp.open("get", "jsp/loginAccount.jsp?userEmailLogin=" + userEmail + "&userPWDLogin=" + userPWD, true);
+        //gửi yêu cầu
+        xmlhttp.send();
+        //Nhận thông báo từ file JSP gửi về
+        /*var rs = xmlhttp.responseText.trim();
+        alert(rs);
+        if (rs == "OK")
+        {
+            //xmlhttp.open("get", "index.html", false);
+            //xmlhttp.send();
+            //document.write(xmlhttp.responseText);
+
+        } else
+            alert("Đăng nhập thất bại, vui lòng kiểm tra lại thông tin...");*/
     }
 }
 
