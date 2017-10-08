c73d8cedbf3e5cb9141907a09fd1158156652937
thieuquangtuan <tuancr261@gmail.com>
2017-10-09 00:32:16 +0700

    build 00:32 09/10/2017 thieuquangtuan

---
+++ build/web/js/processData.js
@@ -0,0 +1,91 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function checkInfoBeforeCreateAccount()
+{
+    var userName = document.getElementById("userName").value;
+    var userEmail = document.getElementById("userEmail").value;
+    var userPWD = document.getElementById("userPWD").value;
+    var userPhone = document.getElementById("userPhone").value;
+    var userAddress = document.getElementById("userAddress").value;
+    if (userName == "" || userEmail == "" || userPWD == "" || userPhone == "" || userAddress == "")
+        alert("Vui lòng nhập đầy đủ thông tin để đăng ký tài khoản...");
+    else
+    {
+        var xmlhttp = new XMLHttpRequest(); //dung de gui DL den webserver
+        //mo kết nối đến Webserver voi yeu cau chay registerAccount.jsp
+        xmlhttp.open("get", "jsp/registerAccount.jsp?userName=" + userName + "&userEmail=" + userEmail + "&userPWD=" + userPWD + "&userPhone=" + userPhone
+                + "&userAddress=" + userAddress, false);
+        //gửi yêu cầu
+        xmlhttp.send();
+        //Nhận thông báo từ file JSP gửi về
+        var rs = xmlhttp.responseText.trim();
+        if (rs === "OK")
+            alert("Đăng ký tài khoản thành công.");
+        else
+            alert("Email bạn nhập vào đã được sử dụng, vui lòng kiểm tra lại...");
+    }
+}
+
+function checkInfoBeforeLogin()
+{
+    var userEmail = document.getElementById("userEmailLogin").value;
+    var userPWD = document.getElementById("userPWDLogin").value;
+    if (userEmail == "" || userPWD == "")
+        alert("Bạn vui lòng nhập đầy đủ thông tin để đăng nhập");
+    else
+    {
+        var xmlhttp = new XMLHttpRequest(); //dung de gui DL den webserver
+        xmlhttp.onreadystatechange = function () { // listen for state changes
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
+            { // when completed we can move away
+                //sessionStorage.username = userEmail;
+                
+                var messenge = xmlhttp.responseText.trim();
+                if (messenge == "OK")
+                {
+                    alert(messenge);
+                    sessionStorage.username = userEmail;
+                    window.location = "index.html";
+                }
+                else
+                    alert("Đăng nhập thất bại, vui lòng kiểm tra lại thông tin...");
+            }
+           
+            
+        }
+        xmlhttp.open("get", "jsp/loginAccount.jsp?userEmailLogin=" + userEmail + "&userPWDLogin=" + userPWD, true);
+        //gửi yêu cầu
+        xmlhttp.send();
+        //Nhận thông báo từ file JSP gửi về
+        /*var rs = xmlhttp.responseText.trim();
+        alert(rs);
+        if (rs == "OK")
+        {
+            //xmlhttp.open("get", "index.html", false);
+            //xmlhttp.send();
+            //document.write(xmlhttp.responseText);
+
+        } else
+            alert("Đăng nhập thất bại, vui lòng kiểm tra lại thông tin...");*/
+    }
+}
+
+function validateLoginForm()
+{
+    var userEmail = document.forms["loginForm"]["userEmailLogin"].value;
+    var userPWD = document.forms["loginForm"]["userPWDLogin"].value;
+    if (userEmail == "" || userPWD == "")
+    {
+        alert("Nhap day du thong tin....");
+        return false;
+    }
+    else
+    {
+        sessionStorage.username = userEmail;
+        return true;
+    }
+    
+}
---
+++ build/web/jsp/loginAccount.jsp
@@ -0,0 +1,37 @@
+<%-- 
+    Document   : registerAccount
+    Created on : Sep 21, 2017, 2:30:15 PM
+    Author     : thieuquangtuan
+--%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.SQLException"%>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%@page import="java.util.*" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    String emailKH = request.getParameter("userEmailLogin");
+    String matkhauKH = request.getParameter("userPWDLogin");
+    String matkhauKHDB = "";
+    //Phải thêm thư viện MySQL vào project trước khi nạp Driver
+    //Nạp Driver của MySQL
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    //Thiết lập kết nối với MySQL
+    Connection con = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/CUAHANG_BANGDIA?useUnicode=true&characterEncoding=utf8", "root", "thematrix141");
+    //Tạo ra đối tượng thực thi các câu lệnh SQL
+    Statement stm = con.createStatement();
+    String queryStm = String.format("select * from KHACHHANG where EmailKH='%s' and PasswordKH='%s'", emailKH, matkhauKH);
+    //out.print(queryStm);
+    ResultSet record = stm.executeQuery(queryStm);
+    if (record.next())
+    {
+        response.sendRedirect("../index.html");
+    } else
+    {
+        out.print("ERORR");
+    }
+    record.close();
+%>
+
